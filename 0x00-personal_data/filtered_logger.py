a6458744e076bf5b7054527628d402330950c49d
jobs-rw <mjabush@yahoo.fr>
2024-02-08 16:44:15 +0000

    commit on updates filtered_logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ Use of regex in replacing occurrences of certain field values """
 import re
+from typing import List
 import logging
 import mysql.connector
 import os
@@ -11,14 +12,14 @@ class RedactingFormatter(logging.Formatter):
     """
 
     REDACTION = "***"
-    FORMAT = "[COMPANY_NAME] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields_to_redact):
+    def __init__(self, fields: List[str]):
         super(RedactingFormatter, self).__init__(self.FORMAT)
-        self.fields = fields_to_redact
+        self.fields = fields
 
-    def format(self, record):
+    def format(self, record: logging.LogRecord) -> str:
         """ Returns filtered values from log records """
         return filter_datum(self.fields, self.REDACTION,
                             super().format(record), self.SEPARATOR)
@@ -27,18 +28,19 @@ class RedactingFormatter(logging.Formatter):
 PII_FIELDS = ("name", "email", "password", "ssn", "phone")
 
 
-def get_database_connection():
+def get_db() -> mysql.connector.connection.MYSQLConnection:
     """ Connection to MySQL environment """
     db_connect = mysql.connector.connect(
-        user=os.getenv('DB_USERNAME', 'root'),
-        password=os.getenv('DB_PASSWORD', ''),
-        host=os.getenv('DB_HOST', 'localhost'),
-        database=os.getenv('DB_NAME')
+        user=os.getenv('PERSONAL_DATA_DB_USERNAME', 'root'),
+        password=os.getenv('PERSONAL_DATA_DB_PASSWORD', ''),
+        host=os.getenv('PERSONAL_DATA_DB_HOST', 'localhost'),
+        database=os.getenv('PERSONAL_DATA_DB_NAME')
     )
     return db_connect
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
     """ Returns regex obfuscated log messages """
     for field in fields:
         message = re.sub(f'{field}=(.*?){separator}',
@@ -46,7 +48,7 @@ def filter_datum(fields, redaction, message, separator):
     return message
 
 
-def get_logger():
+def get_logger() -> logging.Logger:
     """ Returns a logging.Logger object """
     logger = logging.getLogger("user_data")
     logger.setLevel(logging.INFO)
@@ -55,19 +57,19 @@ def get_logger():
     target_handler = logging.StreamHandler()
     target_handler.setLevel(logging.INFO)
 
-    formatter = RedactingFormatter(PII_FIELDS)
-    target_handler.setFormatter(formatter)
+    formatter = RedactingFormatter(list(PII_FIELDS))
+    target_handle.setFormatter(formatter)
 
     logger.addHandler(target_handler)
     return logger
 
 
-def main():
+def main() -> None:
     """ Obtain database connection using get_db
     retrieve all role in the users table and display
     each row under a filtered format
     """
-    db = get_database_connection()
+    db = get_db()
     cursor = db.cursor()
     cursor.execute("SELECT * FROM users;")
 
